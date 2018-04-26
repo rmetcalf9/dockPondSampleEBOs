eb4aff0e9faa5cd608f75c607bd29c75b2f8adb2
Robert Metcalf <rmetcalf9@googlemail.com>
2018-04-26 20:47:00 +0100

    Bugfix

--- EBOs/AnimalsV1/model.py
+++ EBOs/AnimalsV1/model.py
@@ -6,7 +6,7 @@ def getModel(flaskRestPlusAPI):
       'type': fields.String(default='',description='What type of animal are they?'),
       'LikedByRobert': fields.Boolean(default=False,description='Does Robert like this animal?'),
       'genderGUID': fields.String(default='',description='What gender is the animal'),
-      'nextScheduledRun': fields.DateTime(default='',description='When are they next scheduled to run in a race?'),
+      'nextScheduledRun': fields.DateTime(dt_format=u'iso8601',description='When are they next scheduled to run in a race?'),
       'creationDate': fields.DateTime(dt_format=u'iso8601', description='Time job record was created'),
       'lastUpdateDate': fields.DateTime(dt_format=u'iso8601', description='Last time job record was changed (excluding runs)'),
       'lastRunDate': fields.DateTime(dt_format=u'iso8601', description='Last time job record was run')
