77d21abd6a1edcd4f4812db95feabc8dbb60cd84
Robert Metcalf <rmetcalf9@googlemail.com>
2018-04-26 19:05:34 +0100

    Adjusting model template to fit with design

--- EBOs/AnimalsV1/model.py
+++ EBOs/AnimalsV1/model.py
@@ -1,7 +1,7 @@
 from flask_restplus import fields
 
-def getModel(appObj):
-  return appObj.flastRestPlusAPIObject.model('Animal', {
+def getModel(flaskRestPlusAPI):
+  return flaskRestPlusAPI.model('Animal', {
       'name': fields.String(default='',description='What is the name of this animal? (Must be unique)'),
       'type': fields.String(default='',description='What type of animal are they?'),
       'LikedByRobert': fields.Boolean(default=False,description='Does Robert like this animal?'),
