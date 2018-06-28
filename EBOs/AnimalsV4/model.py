b6b68bcaed80915f855346bd6632be9b7f95fbf5
Robert Metcalf <rmetcalf9@googlemail.com>
2018-06-28 13:29:31 +0100

    Updating model

--- EBOs/AnimalsV4/model.py
+++ EBOs/AnimalsV4/model.py
@@ -1,6 +1,21 @@
 from flask_restplus import fields
 
 def getModel(flaskRestPlusAPI):
+  def getTypeModel(flaskRestPlusAPI, typeName):
+    if typeName=='ABC':
+      return flaskRestPlusAPI.model('ABC', {
+          'Nname': fields.String(default='',description='What is the name of this animal? (Must be unique)'),
+          'Ttype': fields.String(default='',description='What type of animal are they?'),
+          'HHatedByRobert': fields.Boolean(default=False,description='Does Robert hate this animal?')
+      })
+    if typeName=='DEF':
+      return flaskRestPlusAPI.model('DEF', {
+          'Nname': fields.String(default='',description='What is the name of this animal? (Must be unique)'),
+          'Nname2222': fields.String(default='',description='What is the name of this animal? (Must be unique)')
+      })
+    raise Exception('Searching for unknown type')
+
+
   return flaskRestPlusAPI.model('Animal', {
       'name': fields.String(default='',description='What is the name of this animal? (Must be unique)'),
       'type': fields.String(default='',description='What type of animal are they?'),
@@ -10,16 +25,3 @@ def getModel(flaskRestPlusAPI):
       'TestNestedListStructure': fields.List(fields.Nested(getTypeModel(flaskRestPlusAPI, 'DEF')))
   })
 
-def getTypeModel(flaskRestPlusAPI, typeName):
-  if typeName=='ABC':
-    return flaskRestPlusAPI.model('ABC', {
-        'Nname': fields.String(default='',description='What is the name of this animal? (Must be unique)'),
-        'Ttype': fields.String(default='',description='What type of animal are they?'),
-        'HHatedByRobert': fields.Boolean(default=False,description='Does Robert hate this animal?')
-    })
-  if typeName=='DEF':
-    return flaskRestPlusAPI.model('DEF', {
-        'Nname': fields.String(default='',description='What is the name of this animal? (Must be unique)'),
-        'Nname2222': fields.String(default='',description='What is the name of this animal? (Must be unique)')
-    })
-  raise Exception('Searching for unknown type')
