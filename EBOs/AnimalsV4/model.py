e25a195a1ef1684a331fe6a73246306aabdfaed2
Robert Metcalf <rmetcalf9@googlemail.com>
2018-06-28 09:06:00 +0100

    Adding new features!

--- EBOs/AnimalsV4/model.py
+++ EBOs/AnimalsV4/model.py
@@ -4,5 +4,22 @@ def getModel(flaskRestPlusAPI):
   return flaskRestPlusAPI.model('Animal', {
       'name': fields.String(default='',description='What is the name of this animal? (Must be unique)'),
       'type': fields.String(default='',description='What type of animal are they?'),
-      'HatedByRobert': fields.Boolean(default=False,description='Does Robert hate this animal?')
+      'HatedByRobert': fields.Boolean(default=False,description='Does Robert hate this animal?'),
+      'TestNestedStructure': fields.Nested(getTypeModel('ABC')),
+      'TestListStructure': fields.list(fields.String(default='',description='list item')),
+      'TestNestedListStructure': fields.list(fields.Nested(getTypeModel('DEF'))
   })
+
+def getTypeModel(typeName):
+  if typeName=='ABC':
+    return flaskRestPlusAPI.model('ABC', {
+        'Nname': fields.String(default='',description='What is the name of this animal? (Must be unique)'),
+        'Ttype': fields.String(default='',description='What type of animal are they?'),
+        'HHatedByRobert': fields.Boolean(default=False,description='Does Robert hate this animal?')
+    })
+  if typeName=='DEF':
+    return flaskRestPlusAPI.model('DEF', {
+        'Nname': fields.String(default='',description='What is the name of this animal? (Must be unique)'),
+        'Nname2222': fields.String(default='',description='What is the name of this animal? (Must be unique)')
+    })
+  raise Exception('Searching for unknown type')
