900bb92678a396f49030d8ab03cc7dbd189b3466
Robert Metcalf <rmetcalf9@googlemail.com>
2018-06-28 09:45:37 +0100

    Bug fix model

--- EBOs/AnimalsV4/model.py
+++ EBOs/AnimalsV4/model.py
@@ -5,12 +5,12 @@ def getModel(flaskRestPlusAPI):
       'name': fields.String(default='',description='What is the name of this animal? (Must be unique)'),
       'type': fields.String(default='',description='What type of animal are they?'),
       'HatedByRobert': fields.Boolean(default=False,description='Does Robert hate this animal?'),
-      'TestNestedStructure': fields.Nested(getTypeModel('ABC')),
-      'TestListStructure': fields.list(fields.String(default='',description='list item')),
-      'TestNestedListStructure': fields.list(fields.Nested(getTypeModel('DEF')))
+      'TestNestedStructure': fields.Nested(getTypeModel(flaskRestPlusAPI, 'ABC')),
+      'TestListStructure': fields.List(fields.String(default='',description='list item')),
+      'TestNestedListStructure': fields.List(fields.Nested(getTypeModel(flaskRestPlusAPI, 'DEF')))
   })
 
-def getTypeModel(typeName):
+def getTypeModel(flaskRestPlusAPI, typeName):
   if typeName=='ABC':
     return flaskRestPlusAPI.model('ABC', {
         'Nname': fields.String(default='',description='What is the name of this animal? (Must be unique)'),
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # dockPond Sample EBOs
 
 [dockPond](https://github.com/rmetcalf9/dockPond) is a application that will create a data lake based on data models it reads from git. This is a sample repository with some data models for it to read.
+
+
+## Code to test a model
+
+Go into the model directory and run the following in a python REPL
+```
+from flask_restplus import Api
+import model
+flaskRestPlusAPIObject = Api(None, version='UNSET', title='Dummy Test', description='Dummy', doc='/', default_mediatype='application/json')
+aa = model.getModel(flaskRestPlusAPIObject)
+```
